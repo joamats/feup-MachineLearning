f46e9bae8cb04a556d0136b5228fa4825cc856a7
joamats <58477967+joamats@users.noreply.github.com>
2020-12-29 15:58:14 +0000

    Prepare models training and assessment for the 10 datasets, for each language

--- model_svm.py
+++ model_svm.py
@@ -9,24 +9,45 @@ Created on Mon Dec 28 01:39:42 2020
     
 """
 
+import numpy as np
 from sklearn import svm
 from data_load import getDataset
 from evaluation_metrics import getMetrics, displayMetrics
 
 #%% Load Datasets
 
-#include a for loop with zip here
-number = 9
-language = "Native"
-x_tr, y_tr, x_val, y_val, x_ts, y_ts = getDataset(number, language)
+languages = ['Native', 'English']
+numLanguages = len(languages)
+numMetrics = 3  # Accuracy, F1-score, Confusion Matrix
+numValues = 2   # Mean, Standard Deviation   
 
-#%% Train Models
+allMetrics = np.zeros((numLanguages, numMetrics, numValues))
 
-model = svm.SVC(kernel = "poly")
-model.fit(x_tr, y_tr)
+for k, language in enumerate(languages):
+    
+    metrics = []
+    
+    for number in range(10):
+        
+        # Get *this* dataset
+        x_tr, y_tr, x_val, y_val, x_ts, y_ts = getDataset(number, language)
+        
+        # Train SVM
+        model = svm.SVC(kernel = "linear")
+        model.fit(x_tr, y_tr)
+        
+        # Assess *this* model
+        metrics.append(getMetrics(model, x_val, y_val))
+        
+    # Mean and Std for Accuracy    
+    allMetrics[k, 0, 0] = np.mean([r[0] for r in metrics])
+    allMetrics[k, 0, 1] = np.std([r[0] for r in metrics])
+    
+    # Mean and Std for F1-score
+    allMetrics[k, 1, 0] = np.mean([r[1] for r in metrics])
+    allMetrics[k, 1, 1] = np.std([r[1] for r in metrics])
+    
+    print('Language:', language)
+    print('Accuracy =', f'{allMetrics[k,0,0]:.3f}', '+/-',  f'{allMetrics[k,0,1]:.3f}')
+    print('F1-Score =', f'{allMetrics[k,1,0]:.3f}', '+/-', f'{allMetrics[k,1,1]:.3f}\n')
 
-#%% Assess Performance
-
-metrics = getMetrics(model, x_val, y_val)
-
-displayMetrics(metrics)
