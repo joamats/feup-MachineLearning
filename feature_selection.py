af523b0ab8684c02a067c72d2263a05699bbda0e
joamats <58477967+joamats@users.noreply.github.com>
2020-12-30 02:00:29 +0000

    Optimization of SVM and Feature Selection implemented

---
+++ feature_selection.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 30 01:10:02 2020
+
+@author: João Matos
+"""
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from matplotlib import pyplot
+from data_load import getDataset
+
+# feature selection
+def select_features(X_train, y_train, X_test):
+	# configure to select all features
+	fs = SelectKBest(score_func=f_classif, k='all')
+	# learn relationship from training data
+	fs.fit(X_train, y_train)
+	# transform train input data
+	X_train_fs = fs.transform(X_train)
+	# transform test input data
+	X_test_fs = fs.transform(X_test)
+	return X_train_fs, X_test_fs, fs
+ 
+languages = ['Native', 'English']   
+for k, language in enumerate(languages):
+    for number in range(10): 
+        
+        x_tr, y_tr, x_val, y_val, x_ts, y_ts = getDataset(number, language, True)
+        
+ 
+        # feature selection
+        x_tr_fs, x_val_fs, fs = select_features(x_tr, y_tr, x_val)
+        # what are scores for the features
+        for i in range(len(fs.scores_)):
+        	print('Feature %d: %f' % (i, fs.scores_[i]))
+        # plot the scores
+        pyplot.bar([i for i in range(len(fs.scores_))], fs.scores_)
+        pyplot.show()
--- model_svm.py
+++ model_svm.py
@@ -13,7 +13,10 @@ import numpy as np
 from timeit import default_timer as timer
 from sklearn import svm
 from data_load import getDataset
-from evaluation_metrics import getMetrics
+from evaluation_metrics import getMetrics, getGeneralMetrics, displayGeneralMetrics
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.pipeline import make_pipeline
 
 start = timer()
 
@@ -21,7 +24,7 @@ start = timer()
 
 languages = ['Native', 'English']
 numLanguages = len(languages)
-numMetrics = 3  # Accuracy, F1-score, Confusion Matrix
+numMetrics = 6  # Accuracy, F1-score, Confusion Matrix
 numValues = 2   # Mean, Standard Deviation   
 
 sMetrics_tr = np.zeros((numLanguages, numMetrics, numValues))
@@ -29,57 +32,75 @@ sMetrics_val = np.zeros((numLanguages, numMetrics, numValues))
 
 # Define Parameters for Train
 
+probability_ = True
 C_ = 100
 kernel_ = 'rbf'
 gamma_ = 0.01
+with_PCA_= True
+with_ANOVA_ = True
+k_features_ = 23
 
 print('SVM Model')
 print('C =', C_)
 print('kernel =', kernel_)
 print('gamma =', gamma_)
+print('With PCA:', with_PCA_)
+print('With ANOVA:', with_ANOVA_)
+if with_ANOVA_:
+    print('k features:', k_features_)
 
 for k, language in enumerate(languages):
     
     metrics_tr = []
     metrics_val = []
+    best_ = []
+
     
     for number in range(10):
         
         # Get *this* dataset
-        x_tr, y_tr, x_val, y_val, x_ts, y_ts = getDataset(number, language)
+        x_tr, y_tr, x_val, y_val, x_ts, y_ts = getDataset(number, language, with_PCA = with_PCA_)
         
-        # Train SVM
+        # SVM model
+        svc = svm.SVC(probability=probability_, random_state=0, C=C_, kernel=kernel_, gamma=gamma_)
         
-        model = svm.SVC(C=C_, kernel=kernel_, gamma=gamma_)
+        if with_ANOVA_:
+            # ANOVA filter, take 3 best features
+            anova_filter = SelectKBest(f_classif, k=k_features_)
+        
+            # Make Pipeline
+            model = make_pipeline(anova_filter, svc)
+        else:
+            model = svc
+    
         model.fit(x_tr, y_tr)
         
-        # Assess *this* model
-        metrics_tr.append(getMetrics(model, x_tr, y_tr))
-        metrics_val.append(getMetrics(model, x_val, y_val))
+        # # defining parameter range 
+        # param_grid = {'anova__k': np.arange(1,30,1)} 
         
-    # Mean and Std for Accuracy    
-    sMetrics_tr[k, 0, 0] = np.mean([r[0] for r in metrics_tr])
-    sMetrics_tr[k, 0, 1] = np.std([r[0] for r in metrics_tr])
-    
-    # Mean and Std for F1-score
-    sMetrics_tr[k, 1, 0] = np.mean([r[1] for r in metrics_tr])
-    sMetrics_tr[k, 1, 1] = np.std([r[1] for r in metrics_tr])
-    
-    # Mean and Std for Accuracy    
-    sMetrics_val[k, 0, 0] = np.mean([r[0] for r in metrics_val])
-    sMetrics_val[k, 0, 1] = np.std([r[0] for r in metrics_val])
-    
-    # Mean and Std for F1-score
-    sMetrics_val[k, 1, 0] = np.mean([r[1] for r in metrics_val])
-    sMetrics_val[k, 1, 1] = np.std([r[1] for r in metrics_val])
+        # grid = GridSearchCV(anova_svm, param_grid, cv = 5, refit = True, verbose = 2)
+        
+        # # fitting the model for grid search 
+        # grid.fit(x_tr, y_tr)
+        
+        # # save the best params
+        # best_.append(grid.best_params_)
+                
+        # Assess *this* model
+        metrics_tr.append(getMetrics(model, x_tr, y_tr, 'withProbs'))
+        metrics_val.append(getMetrics(model, x_val, y_val, 'withProbs'))
+        
+    #get mean and std for each metric
+    sMetrics_tr = getGeneralMetrics(metrics_tr, numMetrics)
+    sMetrics_val = getGeneralMetrics(metrics_val, numMetrics)
     
     print('\nLanguage:', language,'\n')
     print('Training Set')
-    print('Accuracy =', f'{sMetrics_tr[k,0,0]:.3f}', '+/-',  f'{sMetrics_tr[k,0,1]:.3f}')
-    print('F1-Score =', f'{sMetrics_tr[k,1,0]:.3f}', '+/-', f'{sMetrics_tr[k,1,1]:.3f}\n')
+    displayGeneralMetrics(sMetrics_tr)
+    print('\n')
     print('Validation Set')
-    print('Accuracy =', f'{sMetrics_val[k,0,0]:.3f}', '+/-',  f'{sMetrics_val[k,0,1]:.3f}')
-    print('F1-Score =', f'{sMetrics_val[k,1,0]:.3f}', '+/-', f'{sMetrics_val[k,1,1]:.3f}\n')
+    displayGeneralMetrics(sMetrics_val)
+    print('\n')
     
 end = timer()
 print(f'{end-start:.3f}', 's')
